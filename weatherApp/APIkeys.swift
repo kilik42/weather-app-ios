96e1d287add996a2cf050998a6dee9d400b05f24
marvin evins <maevins@cps.edu>
2020-05-29 00:58:01 -0500

    with .gitignore

---
+++ weatherApp/APIkeys.swift
@@ -0,0 +1,14 @@
+//
+//  ApiKeys.swift
+//  weatherApp
+//
+//  Created by marvin evins on 5/29/20.
+//  Copyright © 2020 websavantmedia. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys{
+    static let googlePlacesKey = "AIzaSyCXlZMCPqZKv4oAIemXaHgI4-7Sd42pp_Q"
+    
+}

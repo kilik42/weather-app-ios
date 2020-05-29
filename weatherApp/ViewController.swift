0810d6cc36c256c982a1f574b140af1efaa9ec08
marvin evins <maevins@cps.edu>
2020-05-28 22:39:51 -0500

    initializing weather app project

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by marvin evins on 5/28/20.
+//  Copyright © 2020 websavantmedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
